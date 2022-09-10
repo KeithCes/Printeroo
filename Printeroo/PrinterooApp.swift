4e9ca5101d4588e65d750e791dac9ab598875dbd
Keith Cestaro <34603915+KeithCes@users.noreply.github.com>
2022-09-10 20:03:01 +0300

    fix bug with firebase implementation

--- Printeroo/PrinterooApp.swift
+++ Printeroo/PrinterooApp.swift
@@ -9,12 +9,12 @@ import SwiftUI
 import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-      
-    FirebaseApp.configure()
-    return true
-  }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        return true
+    }
 }
 
 
@@ -22,13 +22,11 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
 struct YourApp: App {
     
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
-  var body: some Scene {
-    WindowGroup {
-      NavigationView {
-        ContentView()
-      }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
     }
-  }
 }
